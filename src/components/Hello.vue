58a32e60ebe3a7f3b33ae6103db9552af6f9f17a
Robin <robin.schreiner@googlemail.com>
2017-09-08 01:29:10 +0200

    Add layout and toolbar

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,11 @@
-<template></template>
+<template>
+  <div class="layout-padding row justify-center">
+    <h1>Welcome to the Quasar Issue Helper</h1>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'hello'
 }
 </script>
-
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // === DEFAULT / CUSTOM STYLE ===
 // WARNING! always comment out ONE of the two require() calls below.
 // 1. use next line to activate CUSTOM STYLE (./src/themes)
-// require(`./themes/app.${__THEME}.styl`)
+require(`./themes/app.${__THEME}.styl`)
 // 2. or, use next line to activate DEFAULT QUASAR STYLE
-require(`quasar/dist/quasar.${__THEME}.css`)
+// require(`quasar/dist/quasar.${__THEME}.css`)
 // ==============================
 
 // Uncomment the following lines if you need IE11/Edge support
@@ -11,18 +11,27 @@ require(`quasar/dist/quasar.${__THEME}.css`)
 // require(`quasar/dist/quasar.ie.${__THEME}.css`)
 
 import Vue from 'vue'
-import Quasar from 'quasar'
+import Quasar, { QBtn, QLayout, QIcon, QToolbar, QToolbarTitle, QTooltip } from 'quasar'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(Quasar) // Install Quasar Framework
+Vue.use(Quasar, {
+  components: {
+    QBtn,
+    QLayout,
+    QIcon,
+    QToolbar,
+    QToolbarTitle,
+    QTooltip
+  }
+}) // Install Quasar Framework
 
 if (__THEME === 'mat') {
   // require('quasar-extras/roboto-font')
 }
 // import 'quasar-extras/material-icons'
 // import 'quasar-extras/ionicons'
-// import 'quasar-extras/fontawesome'
+import 'quasar-extras/fontawesome'
 // import 'quasar-extras/animate'
 
 /* eslint-disable no-new */
