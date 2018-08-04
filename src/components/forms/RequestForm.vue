dcf27b9d905f91bfd619c7dae63fe89c839a8622
panstromek <panstromek@seznam.cz>
2018-08-04 23:08:23 +0200

    change request form styling

--- src/components/forms/RequestForm.vue
+++ src/components/forms/RequestForm.vue
@@ -9,21 +9,16 @@
           requried
       />
     </q-field>
-
-    <div class="row sm-gutter">
-      <div class="col">
-        <q-field helper="What does the proposed API look like?">
-          <q-input
-              v-model="api"
-              type="textarea"
-              :min-rows="7"
-              float-label="Proposed API"
-              color="tertiary"
-              requried
-          />
-        </q-field>
-      </div>
-    </div>
+    <q-field helper="What does the proposed API look like?">
+      <q-input
+          v-model="api"
+          type="textarea"
+          :min-rows="7"
+          float-label="Proposed API"
+          color="tertiary"
+          requried
+      />
+    </q-field>
   </div>
 </template>
 
