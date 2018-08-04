c46e83adcb53cf92ded3e378aa81d3b4f3a7a7ba
panstromek <panstromek@seznam.cz>
2018-08-04 17:06:49 +0200

    fix missing layout

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,26 +1,26 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHr LpR lFr">
     <q-layout-header>
-      <q-toolbar slot="header" color="primary">
+      <q-toolbar color="primary">
         <q-toolbar-title class="row items-center group">
           <img src="~assets/quasar-logo.svg" alt="Quasar Framework Logo" width="50px" height="50px">
           <span>Issue Helper</span>
         </q-toolbar-title>
 
         <q-btn flat @click="openExternal('http://quasar-framework.org/guide/')">
-          <q-icon name="fa-book">
+          <q-icon name="fa fa-book">
             <q-tooltip>Documentation</q-tooltip>
           </q-icon>
         </q-btn>
 
         <q-btn flat @click="openExternal('http://forum.quasar-framework.org/')">
-          <q-icon name="fa-comments">
+          <q-icon name="fa fa-comments">
             <q-tooltip>Forum</q-tooltip>
           </q-icon>
         </q-btn>
 
         <q-btn flat @click="openExternal('https://github.com/quasarframework')">
-          <q-icon name="fa-github">
+          <q-icon name="fab fa-github">
             <q-tooltip>GitHub</q-tooltip>
           </q-icon>
         </q-btn>
