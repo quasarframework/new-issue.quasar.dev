45702992540f10ecc86659dc7af6e2e705bb66cc
panstromek <panstromek@seznam.cz>
2018-08-08 11:40:19 +0200

    request work + update deps

--- src/pages/FeatureRequest.vue
+++ src/pages/FeatureRequest.vue
@@ -1,20 +1,21 @@
 <template>
   <q-page padding>
     <form @submit.prevent="create">
-
       <div class="q-headline q-ma-lg">Feature Request</div>
       <q-field helper="Descriptive - Request must be understood without opening the issue!"
                class="q-pa-lg shadow-1 q-ma-lg">
         <q-input v-model="form.title"
                  placeholder="Add support for windows 95"
                  float-label="Title"
+                 required
                  prefix="[Feature]"/>
       </q-field>
-      <q-field helper="Proposed API" class="q-pa-lg shadow-1 q-ma-lg">
+      <q-field helper="How would developers use this feature?" class="q-pa-lg shadow-1 q-ma-lg">
         <q-input
             v-model="form.api"
             type="textarea"
-            float-label="How to use it?"
+            float-label="Propose API"
+            required
             placeholder="E.g. <new-component :new-prop /> ..."
             :rows="2"
         />
@@ -23,6 +24,7 @@
         <q-input
             v-model="form.problem"
             type="textarea"
+            required
             float-label="Reason add it?"
             placeholder="Convince us..."
             :rows="2"
@@ -31,7 +33,7 @@
       <q-field class="q-pa-lg shadow-1 q-ma-lg">
         <q-checkbox v-model="form.canImplement" label="I can implement this."/>
       </q-field>
-      <q-btn color="primary" class="float-right q-ma-lg" @click="create" type="submit">Create</q-btn>
+      <q-btn color="primary" class="float-right q-ma-lg" @submit.prevent="create" type="submit">Create</q-btn>
     </form>
   </q-page>
 </template>
@@ -77,10 +79,14 @@ export default {
   },
   methods: {
     buildBody () {
-      return this.api + this.problem
+      return '#### Usage\n' +
+        this.form.api +
+        '\n#### Why?\n' +
+        this.form.problem +
+        (this.form.canImplement ? '\n\n#### I Can Implement it.' : 'no')
     },
     buildTitle () {
-      return '[Feature] ' + this.title
+      return '[Feature] ' + this.form.title
     },
     create () {
       let repo = this.repo
