1cc8e547213c8b620677ebd10e5a5ddeb6847f14
panstromek <panstromek@seznam.cz>
2018-08-18 16:58:42 +0200

    reformat

--- src/config/repos.js
+++ src/config/repos.js
@@ -1,14 +1,32 @@
 const repos = [
-  {id: 'quasarframework/quasar', name: 'Quasar'},
-  {id: 'quasarframework/quasar-cli', name: 'Quasar CLI'},
-  {id: 'quasarframework/quasar-play', name: 'Quasar Play'},
-  {id: 'quasarframework/vue-cli-plugin-quasar', name: 'Vue CLI 3 plugin'},
+  {
+    id: 'quasarframework/quasar',
+    name: 'Quasar'
+  },
+  {
+    id: 'quasarframework/quasar-cli',
+    name: 'Quasar CLI'
+  },
+  {
+    id: 'quasarframework/quasar-play',
+    name: 'Quasar Play'
+  },
+  {
+    id: 'quasarframework/vue-cli-plugin-quasar',
+    name: 'Vue CLI 3 plugin'
+  },
   {
     id: 'quasarframework/quasar-extras',
     name: 'Quasar extras (fonts, icons, animations)'
   },
-  {id: 'quasarframework/quasar-starter-kit', name: 'Quasar starter kit'},
-  {id: 'quasarframework/quasar-starter-kit-umd', name: 'UMD starter kit'},
+  {
+    id: 'quasarframework/quasar-starter-kit',
+    name: 'Quasar starter kit'
+  },
+  {
+    id: 'quasarframework/quasar-starter-kit-umd',
+    name: 'UMD starter kit'
+  },
   {
     id: 'quasarframework/quasar-template-meteor',
     name: 'Quasar Meteor starter kit'
