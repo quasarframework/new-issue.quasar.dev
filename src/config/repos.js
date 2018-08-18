e0ca60f69bf8c4239d111c865007364e008565ac
panstromek <panstromek@seznam.cz>
2018-08-18 15:27:52 +0200

    add other repos

--- src/config/repos.js
+++ src/config/repos.js
@@ -1,7 +1,22 @@
 const repos = [
-  { id: 'quasarframework/quasar', name: 'Quasar' },
-  { id: 'quasarframework/quasar-cli', name: 'Quasar CLI' },
-  { id: 'quasarframework/quasar-play', name: 'Quasar Play' }
+  {id: 'quasarframework/quasar', name: 'Quasar'},
+  {id: 'quasarframework/quasar-cli', name: 'Quasar CLI'},
+  {id: 'quasarframework/quasar-play', name: 'Quasar Play'},
+  {id: 'quasarframework/vue-cli-plugin-quasar', name: 'Vue CLI 3 plugin'},
+  {
+    id: 'quasarframework/quasar-extras',
+    name: 'Quasar extras (fonts, icons, animations)'
+  },
+  {id: 'quasarframework/quasar-starter-kit', name: 'Quasar starter kit'},
+  {id: 'quasarframework/quasar-starter-kit-umd', name: 'UMD starter kit'},
+  {
+    id: 'quasarframework/quasar-template-meteor',
+    name: 'Quasar Meteor starter kit'
+  },
+  {
+    id: 'quasarframework/new-issue.quasar-framework.org',
+    name: 'This issue helper'
+  }
 ]
 
 export { repos }
