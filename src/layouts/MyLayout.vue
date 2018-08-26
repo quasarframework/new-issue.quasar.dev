b347ee247b427ce6fedf3438e22d8a6515a6efc5
panstromek <panstromek@seznam.cz>
2018-08-26 14:54:48 +0200

    fix links

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,28 +1,17 @@
 <template>
   <q-layout view="lHr LpR lfr">
     <q-layout-header>
-      <q-toolbar color="primary">
-        <q-toolbar-title class="row items-center group">
-          <img src="~assets/quasar-logo.svg" alt="Quasar Framework Logo" width="50px" height="50px">
-          <span>New Issue Helper</span>
+      <q-toolbar color="primary" class="q-py-sm">
+        <img src="~assets/quasar-logo.svg" alt="Quasar Framework Logo" width="50px" height="50px">
+        <q-toolbar-title>
+          New Issue Helper
         </q-toolbar-title>
 
-        <q-btn flat href="https://quasar-framework.org/guide/" target="_blank" rel="nofollow" type="a"
-               icon="fa fa-book">
-          <q-tooltip>Documentation</q-tooltip>
-        </q-btn>
-        <q-btn flat href="https://discord.gg/5TDhbDg" target="_blank" rel="nofollow" type="a" icon="fab fa-discord">
-          <q-tooltip>Discord</q-tooltip>
-        </q-btn>
-
-        <q-btn flat href="https://forum.quasar-framework.org/" target="_blank" rel="nofollow" type="a"
-               icon="fa fa-comments">
-          <q-tooltip>Forum</q-tooltip>
-        </q-btn>
-
-        <q-btn flat href="https://github.com/quasarframework" target="_blank" rel="nofollow" type="a"
-               icon="fab fa-github">
-          <q-tooltip>GitHub</q-tooltip>
+        <q-btn v-for="link in links" :key="link.link"
+               flat size="lg" class="q-pa-sm"
+               :href="link.link" target="_blank" rel="nofollow" type="a"
+               :icon="link.icon">
+          <q-tooltip>{{link.label}}</q-tooltip>
         </q-btn>
       </q-toolbar>
     </q-layout-header>
@@ -46,6 +35,32 @@
 
 <script>
 export default {
-  name: 'MyLayout'
+  name: 'MyLayout',
+  data () {
+    return {
+      links: [
+        {
+          link: 'https://quasar-framework.org/guide/',
+          label: 'Documentation',
+          icon: 'fa fa-book'
+        },
+        {
+          link: 'https://discord.gg/5TDhbDg',
+          label: 'Discord',
+          icon: 'fab fa-discord'
+        },
+        {
+          link: 'https://forum.quasar-framework.org/',
+          label: 'Forum',
+          icon: 'fa fa-comments'
+        },
+        {
+          link: 'https://github.com/quasarframework',
+          label: 'GitHub',
+          icon: 'fab fa-github'
+        }
+      ]
+    }
+  }
 }
 </script>
